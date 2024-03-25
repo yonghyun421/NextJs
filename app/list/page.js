4cfd8d73a3072ec02ca19df66424b00c775e1fae
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-03-25 16:11:20 +0900

    수량 +, - 기능 추가

--- app/list/page.js
+++ app/list/page.js
@@ -18,7 +18,7 @@ export default function List() {
             onClick={() => {
               let newCount = [...count];
               newCount[index]++;
-              setCount([...newCount]);
+              setCount(newCount);
             }}
           >
             +
@@ -27,7 +27,7 @@ export default function List() {
             onClick={() => {
               let newCount = [...count];
               newCount[index]--;
-              setCount([...newCount]);
+              setCount(newCount);
             }}
           >
             -
